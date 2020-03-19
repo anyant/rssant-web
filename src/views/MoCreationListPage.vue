20f00cf98ab18a203212b850af0eee11051ad077
guyskk <guyskk@qq.com>
2020-03-20 01:12:27 +0800

    add message for clean feed creations

--- src/views/MoCreationListPage.vue
+++ src/views/MoCreationListPage.vue
@@ -5,6 +5,16 @@
         种籽
         <span class="num-total">{{ creations.length }}</span>
       </template>
+      <mu-menu placement="bottom-end" class="action-menu" popover-class="menu-popover">
+        <mu-button icon class="action-menu-button">
+          <mu-icon value="more_vert"></mu-icon>
+        </mu-button>
+        <mu-list slot="content" class="menu-list">
+          <mu-list-item button @click="cleanFeedCreations">
+            <mu-list-item-title>清理种籽</mu-list-item-title>
+          </mu-list-item>
+        </mu-list>
+      </mu-menu>
     </MoBackHeader>
     <div>
       <MoCreationItem
@@ -29,13 +39,13 @@ export default {
   props: {
     vid: {
       type: String,
-      default: '/creation/'
-    }
+      default: '/creation/',
+    },
   },
   computed: {
     creations() {
       return this.$API.feed.creations
-    }
+    },
   },
   mounted() {
     this.$API.syncFeedLoadMushrooms().then(() => {
@@ -45,16 +55,43 @@ export default {
       }
     })
   },
-  methods: {},
+  methods: {
+    cleanFeedCreations() {
+      this.$toast.info('种籽会在24小时后自动清除，暂不支持手动清除')
+    },
+  },
   savePageState() {
     this.$pageState.set('scrollTop', window.scrollY)
     this.$pageState.commit()
-  }
+  },
 }
 </script>
 
 <style lang="less" scoped>
 @import '~@/styles/common';
+
+.action-menu,
+.action-menu-button {
+  width: 32 * @pr;
+  height: 32 * @pr;
+  color: @antTextBlack;
+}
+
+.action-menu {
+  right: -8 * @pr;
+}
+
+.menu-popover .menu-list .mu-item {
+  height: 40 * @pr;
+}
+
+.menu-list {
+  padding: 0 0;
+}
+
+.menu-list .mu-item-title {
+  font-size: 15 * @pr;
+}
 
 .creation-item {
   margin-top: 8 * @pr;
