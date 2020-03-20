126b01ede28f660d1d687d6dcb9926e243df2286
guyskk <guyskk@qq.com>
2020-03-20 16:53:22 +0800

    not redirect to login when network error

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Toast from 'muse-ui-toast';
+import _ from 'lodash'
 
 import routes from './routes'
 import { API } from '@/store'
@@ -17,6 +18,11 @@ router.beforeEach((to, from, next) => {
   const loginRequired = to.matched.some(record => record.meta.loginRequired)
   const goLogin = (error) => {
     let response = error.response
+    // When "Network Error", not redirect to login
+    // https://github.com/axios/axios/issues/383
+    if(_.isNil(response) && _.isNil(error.status)){
+      return goNext()
+    }
     if (response && response.status >= 500) {
       Toast.error(error.message)
       return next(false)
