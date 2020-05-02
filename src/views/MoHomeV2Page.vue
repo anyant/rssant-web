6a4e2016ef522e76f9a3ff7f6cbb5d93abf9276b
guyskk <guyskk@qq.com>
2020-05-02 20:33:17 +0800

    add loading animation to home page

--- src/views/MoHomeV2Page.vue
+++ src/views/MoHomeV2Page.vue
@@ -42,6 +42,13 @@
       </div>
     </MoHeader>
     <div class="main">
+      <div class="list-placeholder-wrapper" v-if="!isReady">
+        <div class="list-placeholder spinner">
+          <div class="bounce1"></div>
+          <div class="bounce2"></div>
+          <div class="bounce3"></div>
+        </div>
+      </div>
       <transition-group
         class="list list-upper"
         name="list"
@@ -84,7 +91,7 @@ import defaultAvatar from '@/assets/avatar.svg'
 import { antRippleGrey } from '@/plugin/common'
 
 const ITEM_HEIGHT = 48
-const PAGE_SIZE = Math.ceil(window.innerHeight / ITEM_HEIGHT)
+const PAGE_SIZE = Math.ceil((window.innerHeight - 48) / ITEM_HEIGHT)
 
 const VirtualItem = Vue.component('VirtualItem', {
   props: {
@@ -144,6 +151,7 @@ export default {
       rippleColor: antRippleGrey,
       openWizard: false,
       wizardTrigger: null,
+      isReady: false,
       virtualUpperHeight: 0,
       virtualUpperList: [],
       virtualLowerHeight: 0,
@@ -172,6 +180,7 @@ export default {
       if (this.$API.feed.isEmpty) {
         this.openWizard = true
       }
+      this.isReady = true
       this.renderVirtualList()
       this.$watch('feedList', () => {
         this.updateVirtualList()
@@ -435,6 +444,73 @@ export default {
 .list-upper {
   display: flex;
   flex-direction: column-reverse;
+}
+
+.list-placeholder-wrapper {
+  position: fixed;
+  top: 48 * @pr;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.list-placeholder {
+  margin: auto;
+  opacity: 0.33;
+}
+
+/** https://tobiasahlin.com/spinkit/ */
+.spinner {
+  width: 70px;
+  text-align: center;
+}
+
+.spinner > div {
+  width: 18px;
+  height: 18px;
+  background-color: darken(@antBackGrey, 20%);
+
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+}
+
+.spinner .bounce1 {
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+
+.spinner .bounce2 {
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+
+@-webkit-keyframes sk-bouncedelay {
+  0%,
+  80%,
+  100% {
+    -webkit-transform: scale(0);
+  }
+  40% {
+    -webkit-transform: scale(1);
+  }
+}
+
+@keyframes sk-bouncedelay {
+  0%,
+  80%,
+  100% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  40% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
 }
 
 .list-enter-active,
