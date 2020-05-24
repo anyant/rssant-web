d2384f2e89b077fa601dfe4a652a748dc7fd1e9d
guyskk <guyskk@qq.com>
2020-05-24 21:20:34 +0800

    fix x-time debug message

--- src/plugin/timeit.js
+++ src/plugin/timeit.js
@@ -3,12 +3,15 @@ import lodash from 'lodash'
 const Timeit = function() {}
 
 Timeit.show = function(time, method, url) {
-  time = time.substring(0, time.length - 2)
-  time = lodash.round(lodash.toNumber(time))
+  let match = /^\d+/i.exec(time)
+  let total_time = -1
+  if (!lodash.isNil(match)) {
+    total_time = lodash.round(lodash.toNumber(match[0]))
+  }
   let color = '#4caf50' // green500
-  if (time > 50) {
+  if (total_time > 50) {
     color = '#f44336' // red500
-  } else if (time > 30) {
+  } else if (total_time > 30) {
     color = '#ff9800' // orange500
   }
   let parent = document.querySelector('#timeit')
@@ -16,7 +19,7 @@ Timeit.show = function(time, method, url) {
   element.style.color = color
   element.innerHTML = `
     <span style="min-width:45px; display:inline-block; text-align:right;">
-      ${time}ms
+      ${total_time}ms
     </span>
     <span>${method.toUpperCase()} ${url}</span>
   `
