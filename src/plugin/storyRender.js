5b692a268649a5c8060585f4b54d2d1ce3d723fd
guyskk <guyskk@qq.com>
2020-07-31 15:52:55 +0800

    fix mathjax render conflicts

--- src/plugin/storyRender.js
+++ src/plugin/storyRender.js
@@ -1,5 +1,9 @@
 import _ from 'lodash'
 
+// Note: use strict regex to check MathJax, use loose regex to render
+// LaTex '$' will conflict with money dollar, jQuery, shell/bash
+// AsciiMath '`' will conflict with shell/bash
+
 // Example:
 // https://liam.page/2019/06/28/variants-of-FM/
 // http://matt33.com/2019/10/27/paper-chandy-lamport/
@@ -8,64 +12,31 @@ import _ from 'lodash'
 // https://timvieira.github.io/blog/post/2016/09/25/evaluating-fx-is-as-fast-as-fx/
 // https://www.zybuluo.com/knight/note/96093
 
-// Note: accept $...$ but not $10...$10, 10$...10$ and jQuery $
 // see also: https://stackoverflow.com/questions/35142364/regex-negative-lookbehind-not-valid-in-javascript
 // see also: https://caniuse.com/#feat=js-regexp-lookbehind
 // see also: https://regexr.com/
-const RE_INLINE_DOLLAR = /([\d(.])?\$(?![(.]).+?([(.])?\$(?![\d(.])/gms
-const RE_INLINE_MATHJAX = /(`[^`]+?`(?!`))|(\\\(.+?\\\))/ms
-const RE_INLINE_NON_AML = /(\\\(.+?\\\))/ms
+
+const RE_STRICT_MATHJAX = /(\$\$.+?\$\$)|(\\\[.+?\\\])|(\\\(.+?\\\))/ms
+const RE_INLINE_MATHJAX = /(\\\(.+?\\\))|(\$.+?\$(?![^\s<]))/ms
 const RE_DISPLAY_MATHJAX = /(\$\$.+?\$\$)|(\\\[.+?\\\])/ms
 
+function hasStrictMathJax(content) {
+  return RE_STRICT_MATHJAX.test(content)
+}
+
 function hasInlineMathJax(content) {
-  if (RE_INLINE_MATHJAX.test(content)) {
-    return true
-  }
-  if (_hasInlineDollar(content)) {
-    return true
-  }
-  return false
-}
-
-function _hasInlineDollar(content) {
-  // JavaScript RegExp objects are stateful when they have the global or sticky flags set
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
-  let inlineDollar = new RegExp(RE_INLINE_DOLLAR)
-  while (true) {
-    let matcher = inlineDollar.exec(content)
-    if (!matcher) {
-      break
-    }
-    if (!matcher[1] && !matcher[2]) {
-      return true
-    }
-  }
-  return false
-}
-
-function hasInlineCodeMathJax(content) {
-  if (RE_INLINE_NON_AML.test(content)) {
-    return true
-  }
-  if (_hasInlineDollar(content)) {
-    return true
-  }
-  return false
+  return RE_INLINE_MATHJAX.test(content)
 }
 
 function hasDisplayMathJax(content) {
   return RE_DISPLAY_MATHJAX.test(content)
 }
 
-function hasMathJax(content) {
-  return hasInlineMathJax(content) || hasDisplayMathJax(content)
-}
-
 function isMathjaxReady() {
   return !_.isNil(window.MathJax) && !_.isNil(window.MathJax.Hub)
 }
 
-export { hasInlineMathJax, hasInlineCodeMathJax, hasDisplayMathJax, hasMathJax }
+export { hasInlineMathJax, hasDisplayMathJax, hasStrictMathJax }
 
 // https://github.com/mathjax/mathjax-docs/wiki/'Can't-make-callback-from-given-data'-error-if-resetEquationNumbers-is-called-when-no-math-is-typeset
 let isFirstMathJaxRender = true
@@ -75,7 +46,7 @@ const StoryRender = {
     function renderMathjax(dom, elementId) {
       dom.querySelectorAll('code,pre').forEach(block => {
         const text = block.innerHTML
-        let hasInline = hasInlineCodeMathJax(text)
+        let hasInline = hasInlineMathJax(text)
         let hasDisplay = hasDisplayMathJax(text)
         if (hasInline || hasDisplay) {
           if (hasInline) {
@@ -117,7 +88,7 @@ const StoryRender = {
       }
       let content = (binding.value || '').trim()
       el.innerHTML = content
-      if (hasMathJax(content)) {
+      if (hasStrictMathJax(content)) {
         renderMathjaxIfReady(el, el.id)
       }
     })
--- tests/unit/storyRender.spec.js
+++ tests/unit/storyRender.spec.js
@@ -1,32 +1,122 @@
+import _ from 'lodash'
 import { expect } from 'chai'
-import { hasInlineMathJax, hasInlineCodeMathJax, hasDisplayMathJax, hasMathJax } from '@/plugin/storyRender'
+import { hasInlineMathJax, hasDisplayMathJax, hasStrictMathJax } from '@/plugin/storyRender'
 
 describe('plugin/storyRender', () => {
+  let trueStrictCases = [
+    '$$1 \\over 3$$',
+    `$$ f(x)=\\left\\{
+      \\begin{aligned}
+      x & = & \\cos(t) \\\\
+      y & = & \\sin(t) \\\\
+      z & = & \\frac xy
+      \\end{aligned}
+      \\right.
+      $$`,
+    `$$\\sqrt[n]{3}$$`,
+    `\\(f(x,y,z) = 3y^2z \\left( 3+\\frac{7x+5}{1+y^2} \\right)\\)`,
+    `a dot product \\(s = w^Tx\\)`,
+    `gradient on the image \\(x\\) is simply \\(\\nabla_x s = w\\).`,
+    `the gradient \\(\\nabla f(x)\\) is as fast of computing \\(f(x)\\).`,
+  ]
+  trueStrictCases = _.concat(
+    trueStrictCases,
+    trueStrictCases.map(x => x.replace(/$$/g, '\\(')),
+    trueStrictCases.map(x => x.replace(/$$/g, '\\['))
+  )
+  trueStrictCases = _.concat(
+    trueStrictCases,
+    trueStrictCases.map(x => '<code>' + x + '</code>')
+  )
+  let falseStrictCases = [
+    '$1 \\over 3$',
+    '$x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}$',
+    '$10 aaa $10  $10 aaa $10',
+    '$10.0',
+    '100$ aaa 100$ $100 aaa $100',
+    'when `x < y` we have',
+    `<p>这里 $n$ 是特征`,
+    `向量 $\\vec x$ 的长度，即特征的维数。`,
+    `<code>$v_i$</code> 是长度`,
+    `为 $k$ 的向量，与特征 id 对应，称为特征的隐向量。`,
+    'console.log($.fn.jquery); window.$;',
+    '$ === jQuery; typeof($);',
+    "$('p,div'); $('p.red,p.green');",
+    `$ shellcheck test.sh
+      In test.sh line 4:
+      if[ $# -eq 0 ]`,
+    `$ shellcheck if[ $# -eq 0 ]`,
+    '`sum_(i=1)^n i^3=((n(n+1))/2)^2`',
+    '`hello world`',
+    "echo `date '--date=1 hour ago' +%Y-%m-%d-%H`",
+  ]
   let trueInlineCases = [
     '$x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}$',
     '$f(x,y,z) = 3y^2z \\left( 3+\\frac{7x+5}{1+y^2} \\right)$',
     '$1 \\over 3$',
+    '$\\sqrt[n]{3}$',
     '$\\vec{a} \\cdot \\vec{b}=0$',
     '\\(\\sqrt[n]{3}\\)',
-    '$\\sqrt[n]{3}$',
+    `\\(\\sqrt[n]{3}\\)`,
   ]
   let falseInlineCases = [
     '$10 aaa $10  $10 aaa $10',
     '$10 $10  $10 $10',
     '$10.0',
-    '100$ 100$',
     'console.log($.fn.jquery); window.$;',
     '$ === jQuery; typeof($);',
     "$('p,div'); $('p.red,p.green');",
+    `$ shellcheck test.sh
+      In test.sh line 4:
+      if[ $# -eq 0 ]`,
+    `$ shellcheck if[ $# -eq 0 ]`,
+    `the cost is $2.50 for the first one, and $2.00 for each additional one`,
+    `The model of subscription premium audio content is popular in China,
+      where Ximalaya, a unicorn consumer audio platform, has a subscription
+      feature for $3 monthly that enables users to access over 4000 e-books
+      and over 300 premium audio courses or podcasts. Audio content is also
+      available a la carte starting at $0.03 per short, serialized book chapter,
+      or anywhere from $10 to $45 for paid audio courses.`,
+    '```hello world```',
+    "echo $(date '--date=1 hour ago' +%Y-%m-%d-%H)",
   ]
-  let asciiMathCases = [
-    '`sum_(i=1)^n i^3=((n(n+1))/2)^2`',
-    '`hello world`',
-    "echo `date '--date=1 hour ago' +%Y-%m-%d-%H`",
+  let trueDisplayCases = [
+    `$$ f(x)=\\left\\{
+      \\begin{aligned}
+      x & = & \\cos(t) \\\\
+      y & = & \\sin(t) \\\\
+      z & = & \\frac xy
+      \\end{aligned}
+      \\right.
+      $$`,
+    `\\[\\sqrt[n]{3}\\]`,
+    `$$\\sqrt[n]{3}$$`,
+  ]
+  trueDisplayCases = _.concat(
+    trueDisplayCases,
+    trueDisplayCases.map(x => x.replace(/$$/, '\\('))
+  )
+  trueDisplayCases = _.concat(
+    trueDisplayCases,
+    trueDisplayCases.map(x => '<code>' + x + '</code>')
+  )
+  let falseDisplayCases = [
+    `\\(x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}\\)`,
+    `$x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}$`,
+    `\\{x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}\\}`,
   ]
 
+  it('test hasStrictMathJax', () => {
+    trueStrictCases.forEach(text => {
+      expect(hasStrictMathJax(text)).to.equal(true, text)
+    })
+    falseStrictCases.forEach(text => {
+      expect(hasStrictMathJax(text)).to.equal(false, text)
+    })
+  })
+
   it('test hasInlineMathJax', () => {
-    ;[...trueInlineCases, ...asciiMathCases].forEach(text => {
+    trueInlineCases.forEach(text => {
       expect(hasInlineMathJax(text)).to.equal(true, text)
     })
     falseInlineCases.forEach(text => {
@@ -34,70 +124,12 @@ describe('plugin/storyRender', () => {
     })
   })
 
-  it('test hasInlineCodeMathJax', () => {
-    trueInlineCases.forEach(text => {
-      expect(hasInlineCodeMathJax(text)).to.equal(true, text)
-    })
-    ;[...falseInlineCases, ...asciiMathCases].forEach(text => {
-      expect(hasInlineCodeMathJax(text)).to.equal(false, text)
-    })
-  })
-
   it('test hasDisplayMathJax', () => {
-    let trueCases = [
-      `$$ f(x)=\\left\\{
-       \\begin{aligned}
-       x & = & \\cos(t) \\\\
-       y & = & \\sin(t) \\\\
-       z & = & \\frac xy
-       \\end{aligned}
-       \\right.
-       $$`,
-      `\\[\\sqrt[n]{3}\\]`,
-      `$$\\sqrt[n]{3}$$`,
-    ]
-    trueCases.forEach(text => {
+    trueDisplayCases.forEach(text => {
       expect(hasDisplayMathJax(text)).to.equal(true, text)
     })
-    let falseCases = [
-      `\\(x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}\\)`,
-      `$x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}$`,
-      `\\{x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}\\}`,
-    ]
-    falseCases.forEach(text => {
+    falseDisplayCases.forEach(text => {
       expect(hasDisplayMathJax(text)).to.equal(false, text)
-    })
-  })
-
-  it('test hasMathJax', () => {
-    let trueCases = [
-      `$x^{y^z}=(1+{\\rm e}^x)^{-2xy^w}$`,
-      `\\(f(x,y,z) = 3y^2z \\left( 3+\\frac{7x+5}{1+y^2} \\right)\\)`,
-      'when `x < y` we have',
-      `<p>这里 $n$ 是特征`,
-      `向量 $\\vec x$ 的长度，即特征的维数。`,
-      `<code>$v_i$</code> 是长度`,
-      `为 $k$ 的向量，与特征 id 对应，称为特征的隐向量。`,
-      `a dot product \\(s = w^Tx\\)`,
-      `gradient on the image \\(x\\) is simply \\(\\nabla_x s = w\\).`,
-      `the gradient \\(\\nabla f(x)\\) is as fast of computing \\(f(x)\\).`,
-    ]
-    ;[...trueCases, ...asciiMathCases].forEach(text => {
-      expect(hasMathJax(text)).to.equal(true, text)
-    })
-    let falseCases = [
-      `the cost is $2.50 for the first one, and $2.00 for each additional one`,
-      `The model of subscription premium audio content is popular in China,
-       where Ximalaya, a unicorn consumer audio platform, has a subscription
-       feature for $3 monthly that enables users to access over 4000 e-books
-       and over 300 premium audio courses or podcasts. Audio content is also
-       available a la carte starting at $0.03 per short, serialized book chapter,
-       or anywhere from $10 to $45 for paid audio courses.`,
-      '```hello world```',
-      "echo $(date '--date=1 hour ago' +%Y-%m-%d-%H)",
-    ]
-    falseCases.forEach(text => {
-      expect(hasMathJax(text)).to.equal(false, text)
     })
   })
 })
