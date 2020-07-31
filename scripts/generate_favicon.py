1a8d5039bcf909c2e72f9c0999780b3ee728d3fc
guyskk <guyskk@qq.com>
2020-07-31 18:47:16 +0800

    reduce favicon size

--- scripts/generate_favicon.py
+++ scripts/generate_favicon.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 filepath = 'design/rssant-logo.png'
 output = 'public/favicon.ico'
-icon_sizes = [(128, 128)]
+icon_size = (128, 128)
 
 img = Image.open(filepath)
-img.save(output, sizes=icon_sizes)
+img.resize(icon_size, Image.HAMMING).save(output, sizes=[icon_size])
