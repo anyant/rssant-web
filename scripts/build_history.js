6f8ec4f9036ddf48e6af7e36cf2a519500172b55
guyskk <guyskk@qq.com>
2021-12-30 18:04:28 +0800

    fix build history copy dir

--- scripts/build_history.js
+++ scripts/build_history.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const fs = require('fs')
 const { execSync } = require('child_process')
+const path = require('path')
 
 function sh(command) {
   execSync(command, { stdio: 'inherit' })
@@ -10,9 +11,34 @@ function sh(command) {
 function getBuildList({ dirPath, keepCount }) {
   let filenameList = fs.readdirSync(dirPath)
   let buildList = filenameList.filter(x => x !== 'merged').sort()
-  let outdatedList = buildList.slice(0, buildList.length - keepCount)
-  let keepList = buildList.slice(buildList.length - keepCount, buildList.length)
+  let keepIndex = Math.max(0, buildList.length - keepCount)
+  let outdatedList = buildList.slice(0, keepIndex)
+  let keepList = buildList.slice(keepIndex, buildList.length)
   return { outdated: outdatedList, keep: keepList }
+}
+
+/** 复制文件到目标路径，合并同名的目录，跳过同名的文件 */
+function copyBuildToTarget(source, target) {
+  if (fs.existsSync(target)) {
+    if (!fs.lstatSync(target).isDirectory()) {
+      return
+    }
+    if (!fs.lstatSync(source).isDirectory()) {
+      return
+    }
+    let filenameList = fs.readdirSync(source)
+    for (let filename of filenameList) {
+      copyBuildToTarget(path.join(source, filename), path.join(target, filename))
+    }
+  } else {
+    let sourceInfo = fs.lstatSync(source)
+    if (sourceInfo.isDirectory()) {
+      fs.mkdirSync(target)
+      copyBuildToTarget(source, target)
+    } else if (sourceInfo.isFile()) {
+      fs.copyFileSync(source, target)
+    }
+  }
 }
 
 const DIST_DIR = './dist'
@@ -32,7 +58,7 @@ function main() {
   if (fs.existsSync(buildDir)) {
     fs.rmSync(buildDir, { recursive: true })
   }
-  fs.cpSync(DIST_DIR, buildDir, { recursive: true })
+  copyBuildToTarget(DIST_DIR, buildDir)
 
   const buildList = getBuildList({ dirPath: HISTORY_DIR, keepCount: 10 })
   if (buildList.outdated.length > 0) {
@@ -48,9 +74,10 @@ function main() {
     fs.rmSync(mergedDir, { recursive: true })
   }
   fs.mkdirSync(mergedDir, { recursive: true })
-  for (let filename of buildList.keep) {
+  let keepBuildList = buildList.keep.slice().reverse()
+  for (let filename of keepBuildList) {
     console.log(`[INFO] merge ${filename}`)
-    fs.cpSync(`${HISTORY_DIR}/${filename}`, mergedDir, { recursive: true })
+    copyBuildToTarget(`${HISTORY_DIR}/${filename}`, mergedDir)
   }
 
   sh(`ls -lah ${HISTORY_DIR}`)
