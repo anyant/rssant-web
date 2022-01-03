02f91958919315646736c162ffae0ddc46f252b6
guyskk <guyskk@qq.com>
2022-01-03 23:24:53 +0800

    update local https server

--- scripts/serve_dist.js
+++ scripts/serve_dist.js
@@ -1,32 +1,23 @@
 /* eslint-disable no-console */
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const https = require('https');
-const express = require('express');
-const proxy = require('http-proxy-middleware');
-const proxyConfig = require('../vue.config.js').devServer.proxy;
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const proxy = require('http-proxy-middleware')
+const proxyConfig = require('../vue.config.js').devServer.proxy
 
-var privateKey = fs.readFileSync('scripts/sslcert/key.pem', 'utf8');
-var certificate = fs.readFileSync('scripts/sslcert/cert.pem', 'utf8');
-var credentials = { key: privateKey, cert: certificate };
+var app = express()
 
-var app = express();
-
-Object.keys(proxyConfig).forEach(function (subpath) {
-    app.use(subpath, proxy(subpath, proxyConfig[subpath]));
-});
-app.use(express.static('dist'));
+Object.keys(proxyConfig).forEach(function(subpath) {
+  app.use(subpath, proxy(subpath, proxyConfig[subpath]))
+})
+app.use(express.static('dist'))
 
 /* final catch-all route to index.html defined last */
 app.get('/*', (req, res) => {
-    res.sendFile(path.resolve('dist/index.html'));
+  res.sendFile(path.resolve('dist/index.html'))
 })
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+var httpServer = http.createServer(app)
 
 console.log('Listen HTTP: http://localhost:6789')
-httpServer.listen(6789);
-console.log('Listen HTTPs: https://localhost:6783')
-httpsServer.listen(6783);
+httpServer.listen(6789)
