24328f8a2ba32c352d4b1d9b3493ca58f479ca7e
guyskk <guyskk@qq.com>
2024-08-24 10:17:09 +0800

    update build script and nginx config

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,7 +5,10 @@ set -ex
 if [ -e deploy/history ]; then
     rm -r deploy/history
 fi
-docker build -f deploy/build.Dockerfile -t rssant/web-build:latest .
+docker build --platform linux/amd64 \
+    -f deploy/build.Dockerfile \
+    -t rssant/web-build:latest \
+    .
 
 HISTORY_DIR=$1
 if [ -z "$HISTORY_DIR" ]; then
@@ -15,8 +18,13 @@ else
 fi
 mkdir -p "$HISTORY_DIR"
 
-docker run -ti --rm -v "$HISTORY_DIR":/app/tmp/history rssant/web-build:latest
+docker run --platform linux/amd64 -ti --rm \
+    -v "$HISTORY_DIR":/app/tmp/history \
+    rssant/web-build:latest
 
 mkdir -p deploy/history
 cp -r "$HISTORY_DIR"/merged deploy/history/merged
-docker build -f deploy/web.Dockerfile -t rssant/web:latest .
+docker build --platform linux/amd64 \
+    -f deploy/web.Dockerfile \
+    -t rssant/web:latest \
+    .
